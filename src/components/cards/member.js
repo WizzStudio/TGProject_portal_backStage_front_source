116230a06fd79d9dd58a0cc6a1939c52002bc43c
ACERY1 <y841034081@gmail.com>
2017-12-14 11:11:46 +0800

    add datePlugin & finish delMember

---
+++ src/components/cards/member.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
+import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import {departments} from "../../pages/departments";
+
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 20,
+  },
+};
+
+
+let departmentFilter = (id) => {
+  switch (id) {
+    case 1:
+      return '工程师';
+    case 2:
+      return '设计师';
+    case 3:
+      return '产品';
+  }
+}
+
+function MemberCard(props, delFn = () => {
+}) {
+  // let tags = JSON.parse(props.tag);
+  
+  
+  const handleClick = () => {
+    delFn(props.id)
+  };
+  
+  return (
+    <div>
+      <br/>
+      <Card style={styles.card}>
+        <CardMedia
+          style={styles.media}
+          image={props.avatar}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography component="p">
+            {props.username}
+          </Typography>
+          
+          <Typography component="p">
+            {departmentFilter(props.departmentId)}
+          </Typography>
+          {/*<Typography component="p">*/}
+          {/*{props.introduction}*/}
+          {/*</Typography>*/}
+        </CardContent>
+        {/*{tags.map(n => {*/}
+        {/*return (*/}
+        {/*<Button color="accent">*/}
+        {/*{n}*/}
+        {/*</Button>*/}
+        {/*)*/}
+        {/*})}*/}
+        <Button color="accent" onClick={handleClick}>
+          删除
+        </Button>
+      </Card>
+      <br/>
+    </div>
+  );
+}
+
+
+export default MemberCard;
--- src/pages/projectInfo.js
+++ src/pages/projectInfo.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
+import {DateRangePicker, SingleDatePicker, DayPickerRangeController} from 'react-dates';
 import {observer, inject} from 'mobx-react';
 import {getProjectById} from '../service/API'
 import MenuItem from 'material-ui/Menu/MenuItem';
 import TextField from 'material-ui/TextField';
 import Grid from 'material-ui/Grid';
 import {parseTime} from '../common/scripts/utils'
+import Member from '../components/cards/member'
+import {members} from "./members";
 
-
-const infoStyle={
-  boxPadding:{
-    padding:"20px"
+const infoStyle = {
+  boxPadding: {
+    padding: "20px"
   }
 };
 
 
-
 @inject('appState')
 @observer
-class projectInfo extends React.Component{
+class projectInfo extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      info:{
+    this.state = {
+      info: {
         "id": 3,
         "projectName": "基于大数据的医疗数据分析系统",
         "leaderId": 7,
@@ -50,79 +53,121 @@ class projectInfo extends React.Component{
         "picUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511249995682&di=cd4f49399896958a9f203c2b29ae550f&imgtype=0&src=http%3A%2F%2Fwww.36dsj.com%2Fwp-content%2Fuploads%2F2015%2F03%2F514.jpg",
         "categories": null,
         "uiprogress": 0
+      },
+      startDate: null,
+      endDate: null
+    }
+  }
+  
+  popMember = (id) => {
+    // 状态机特性，保存改变状态前的状态 状态目前是原子性改变，保存上一步的状态进行改变，然后统一去触发视图更新
+    let preInfo = this.state.info;
+    for (let i = 0; i < preInfo.members.length; ++i) {
+      if (preInfo.members[i].id == id) {
+        preInfo.members.splice(i,1)
       }
     }
-  }
-  
-  componentWillMount(){
-    if(!this.props.appState.projectId){
-      return this.props.history.push('/projects')
-    }
-    getProjectById(this.props.appState.projectId).then(data =>{
-      this.setState({
-        info:data
-      })
-    })
-  }
-  
-  handleChange=key=>event =>{
-    // console.log(event.target.value)
     this.setState({
-      info:Object.assign({
-        [key]:event.target.value
-      })
+      info:preInfo
     })
   };
   
-  renderInput = (keyName,label)=>(
+  componentWillMount() {
+    if (!this.props.appState.projectId) {
+      return this.props.history.push('/projects')
+    }
+    getProjectById(this.props.appState.projectId).then(data => {
+      this.setState({
+        info: data
+      })
+    })
+  }
+  
+  handleChange = key => event => {
+    // console.log(event.target.value)
+    let preInfo = this.state.info
+    preInfo[key]=  event.target.value
+    
+    this.setState({
+      info:preInfo
+    })
+  };
+  
+  renderInput = (keyName, label) => (
     <TextField
       id={keyName}
       label={label}
-      value={this.state.info[keyName]}
+      value={this.state.info[keyName] || ''}
       onChange={this.handleChange(`${keyName}`)}
       margin="normal"
       fullWidth
     />
   )
   
-  render (){
+  render() {
     return (
       <div style={infoStyle.boxPadding}>
-        {this.renderInput('projectName','项目名')}
+        {this.renderInput('projectName', '项目名')}
         <br/>
-        {this.renderInput('introduction','项目介绍')}
-        <br/>
-        {this.renderInput('leaderId','项目负责人Id')}
-        <br/>
-        {this.renderInput('startDate','开始时间')} {/*date*/}
-        <br/>
-        {this.renderInput('finishedDate','结束时间')} {/*date*/}
-        <br/>
-        {this.renderInput('sourceUrl','仓库地址')}
-        <br/>
-        {this.renderInput('frontProgress','前端进度')}
-        <br/>
-        {this.renderInput('backProgress','后端进度')}
-        <br/>
-        {this.renderInput('uiProgress','UI进度')}
-        <br/>
-        {this.renderInput('totalProgress','总进度')}
-        <br/>
-        {this.renderInput('picUrl','图片链接')}
-        <br/>
-        {this.renderInput('categories','分类')} {/*list*/}
-        <br/>
-        {this.renderInput('members','成员')} {/*list*/}
+        {this.renderInput('introduction', '项目介绍')}
         <br/>
         
+        <Grid container spacing={24}>
+          <Grid item lg={6}>
+            {this.renderInput('leaderId', '项目负责人Id')}
+          </Grid>
+          <Grid item lg={6} justify="center">
+            <p>开始时间和结束时间</p>
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              onDatesChange={({startDate, endDate}) => this.setState({
+                startDate, endDate
+              })} // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={focusedInput => this.setState({focusedInput})} // PropTypes.func.isRequired,
+              startDateId={'1'}
+              endDateId={'2'}
+              showDefaultInputIcon={true}
+            />
+          </Grid>
+        </Grid>
+        <br/>
+        {/*{this.renderInput('startDate','开始时间')} /!*date*!/*/}
+        {/*<br/>*/}
+        {/*{this.renderInput('finishedDate','结束时间')} /!*date*!/*/}
+        <br/>
+        {this.renderInput('sourceUrl', '仓库地址')}
+        <br/>
+        {this.renderInput('frontProgress', '前端进度')}
+        <br/>
+        {this.renderInput('backProgress', '后端进度')}
+        <br/>
+        {this.renderInput('uiProgress', 'UI进度')}
+        <br/>
+        {this.renderInput('totalProgress', '总进度')}
+        <br/>
+        {this.renderInput('picUrl', '图片链接')}
+        <br/>
+        {this.renderInput('categories', '分类')} {/*list*/}
+        
+        <Grid container spacing={12}>
+          {this.state.info.members.map(n => (
+            <Grid item lg={2}>
+              {Member(n, this.popMember)}
+            </Grid>
+          ))}
+        </Grid>
+      
+      
       </div>
     )
   }
 }
 
 
-projectInfo.propTypes={
-  data:PropTypes.array
+projectInfo.propTypes = {
+  data: PropTypes.array
 }
 
 export default projectInfo
